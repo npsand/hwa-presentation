6c65644f616e40bf495eb02708b3106bddcdda4f
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2023-02-03 15:10:29 +0200

    removed unneeded files, added better object detection

--- install_requirements.py
+++ install_requirements.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+import find_version
 import platform
-import sys, os, subprocess
+import sys
+import os
+import subprocess
 import argparse
 import re
 import platform
 
 convert_default = "empty"
 parser = argparse.ArgumentParser()
-parser.add_argument('-sdai', "--skip_depthai", action="store_true", help="Skip installation of depthai library.")
-parser.add_argument('-dr', "--dry_run", action="store_true", help="Print commands without executing.")
-parser.add_argument("--convert", nargs="?", default=convert_default, help="Convert the NN blobs using BlobConverter. Can be used as --convert 2021.4 to convert using OpenVINO 2021.4 or just --convert to use latest OpenVINO release")
+parser.add_argument('-sdai', "--skip_depthai", action="store_true",
+                    help="Skip installation of depthai library.")
+parser.add_argument('-dr', "--dry_run", action="store_true",
+                    help="Print commands without executing.")
+parser.add_argument("--convert", nargs="?", default=convert_default,
+                    help="Convert the NN blobs using BlobConverter. Can be used as --convert 2021.4 to convert using OpenVINO 2021.4 or just --convert to use latest OpenVINO release")
+
 
 def prettyPrint(command):
 
@@ -18,7 +25,7 @@ def prettyPrint(command):
 
     stringBuilder = str()
     for i, item in enumerate(command):
-        if(hasWhitespace(item)):
+        if (hasWhitespace(item)):
             stringBuilder += ' "' + item + '"'
         else:
             if i == 0:
@@ -36,7 +43,6 @@ examples_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(parent_dir)
 sys.path.insert(1, parent_dir)
-import find_version
 
 # 3rdparty dependencies to install
 DEPENDENCIES = ['pyyaml', 'requests']
@@ -60,12 +66,12 @@ if requireOpenCv:
         DEPENDENCIES.append('opencv-python')
 
 
-
 # Constants
 ARTIFACTORY_URL = 'https://artifacts.luxonis.com/artifactory/luxonis-python-snapshot-local'
 
 # Check if in virtual environment
-in_venv = getattr(sys, "real_prefix", getattr(sys, "base_prefix", sys.prefix)) != sys.prefix
+in_venv = getattr(sys, "real_prefix", getattr(
+    sys, "base_prefix", sys.prefix)) != sys.prefix
 pip_call = [sys.executable, "-m", "pip"]
 pip_installed = True
 pip_install = pip_call + ["install", "-U"]
@@ -81,10 +87,11 @@ if not pip_installed:
     raise RuntimeError(err_str)
 
 if sys.version_info[0] != 3:
-    raise RuntimeError("Examples require Python 3 to run (detected: Python {})".format(sys.version_info[0]))
+    raise RuntimeError(
+        "Examples require Python 3 to run (detected: Python {})".format(sys.version_info[0]))
 
 is_pi = thisPlatform.startswith("arm")
-prebuiltWheelsPythonVersion = [7,9]
+prebuiltWheelsPythonVersion = [7, 9]
 if requireOpenCv and is_pi and sys.version_info[1] not in prebuiltWheelsPythonVersion:
     print("[WARNING] There are no prebuilt wheels for Python 3.{} for OpenCV, building process on this device may be long and unstable".format(sys.version_info[1]))
 
@@ -111,15 +118,18 @@ if not args.skip_depthai:
     git_commit = ""
     git_branch = ""
     try:
-        git_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('UTF-8').strip()
-        git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('UTF-8').strip()
+        git_commit = subprocess.check_output(
+            ['git', 'rev-parse', 'HEAD']).decode('UTF-8').strip()
+        git_branch = subprocess.check_output(
+            ['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('UTF-8').strip()
     except (OSError, subprocess.CalledProcessError) as e:
         git_context = False
 
     # Install depthai depending on context
     if not git_context or git_branch == 'main':
         # Install latest pypi depthai release
-        depthai_install_cmd = [*pip_package_install, '-U', '--force-reinstall', 'depthai']
+        depthai_install_cmd = [*pip_package_install,
+                               '-U', '--force-reinstall', 'depthai']
         if args.dry_run:
             prettyPrint(depthai_install_cmd)
         else:
@@ -127,7 +137,8 @@ if not args.skip_depthai:
 
     elif git_context:
         try:
-            subprocess.check_output(['git', 'submodule', 'update', '--init', '--recursive'])
+            subprocess.check_output(
+                ['git', 'submodule', 'update', '--init', '--recursive'])
         except (OSError, subprocess.CalledProcessError) as e:
             print("git submodule update failed!")
             raise
@@ -151,10 +162,12 @@ if not args.skip_depthai:
 
         # If all commands failed
         if not success:
-            print("Couldn't install dependencies as wheels and trying to compile from sources failed")
+            print(
+                "Couldn't install dependencies as wheels and trying to compile from sources failed")
             print("Check https://github.com/luxonis/depthai-python#dependencies on retrieving dependencies for compiling from sources")
 
-downloader_cmd = [sys.executable, f"{examples_dir}/downloader/downloader.py", "--all", "--cache_dir", f"{examples_dir}/downloader/", "--num_attempts", "5", "-o", f"{examples_dir}/models"]
+downloader_cmd = [sys.executable, f"{examples_dir}/downloader/downloader.py", "--all", "--cache_dir",
+                  f"{examples_dir}/downloader/", "--num_attempts", "5", "-o", f"{examples_dir}/models"]
 if args.dry_run:
     prettyPrint(downloader_cmd)
 else:
@@ -168,7 +181,8 @@ if args.convert != convert_default:
         "yolo-v3-tiny-tf": [6],
     }
     blobconverter_cmds = [
-        [sys.executable, "-m", "blobconverter", "-zn", nn_name, "-sh", str(nn_shave), "-o", f"{examples_dir}/models", *(["-v", args.convert] if args.convert is not None else [])]
+        [sys.executable, "-m", "blobconverter", "-zn", nn_name, "-sh",
+            str(nn_shave), "-o", f"{examples_dir}/models", *(["-v", args.convert] if args.convert is not None else [])]
         for nn_name in nn_models_shaves
         for nn_shave in nn_models_shaves[nn_name]
     ]
@@ -183,9 +197,9 @@ if requireOpenCv and thisPlatform == "aarch64":
     from os import environ
     OPENBLAS_CORE_TYPE = environ.get('OPENBLAS_CORE_TYPE')
     if OPENBLAS_CORE_TYPE != 'ARMV8':
-        WARNING='\033[1;5;31m'
-        RED='\033[91m'
-        LINE_CL='\033[0m'
-        SUGGESTION='echo "export OPENBLAS_CORETYPE=ARMV8" >> ~/.bashrc && source ~/.bashrc'
+        WARNING = '\033[1;5;31m'
+        RED = '\033[91m'
+        LINE_CL = '\033[0m'
+        SUGGESTION = 'echo "export OPENBLAS_CORETYPE=ARMV8" >> ~/.bashrc && source ~/.bashrc'
         print(f'{WARNING}WARNING:{LINE_CL} Need to set OPENBLAS_CORE_TYPE environment variable, otherwise opencv will fail with illegal instruction.')
         print(f'Run: {RED}{SUGGESTION}{LINE_CL}')
--- person_test.py
+++ person_test.py
@@ -9,10 +9,13 @@ import argparse
 
 labelMap = ["person"]
 
-nnPathDefault = str((Path(__file__).parent / Path('./mobilenet-ssd_openvino_2021.4_6shave.blob')).resolve().absolute())
+nnPathDefault = str((Path(__file__).parent / Path(
+    './mobilenet-ssd_openvino_2021.4_6shave.blob')).resolve().absolute())
 parser = argparse.ArgumentParser()
-parser.add_argument('nnPath', nargs='?', help="Path to mobilenet detection network blob", default=nnPathDefault)
-parser.add_argument('-ff', '--full_frame', action="store_true", help="Perform tracking on full RGB frame", default=False)
+parser.add_argument('nnPath', nargs='?',
+                    help="Path to mobilenet detection network blob", default=nnPathDefault)
+parser.add_argument('-ff', '--full_frame', action="store_true",
+                    help="Perform tracking on full RGB frame", default=False)
 
 args = parser.parse_args()
 
@@ -23,13 +26,17 @@ pipeline = dai.Pipeline()
 
 # Define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-detectionNetwork = pipeline.create(dai.node.MobileNetDetectionNetwork)
+spatialDetectionNetwork = pipeline.create(
+    dai.node.MobileNetSpatialDetectionNetwork)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
 objectTracker = pipeline.create(dai.node.ObjectTracker)
 
-xlinkOut = pipeline.create(dai.node.XLinkOut)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
 trackerOut = pipeline.create(dai.node.XLinkOut)
 
-xlinkOut.setStreamName("preview")
+xoutRgb.setStreamName("preview")
 trackerOut.setStreamName("tracklets")
 
 # Properties
@@ -37,31 +44,53 @@ camRgb.setPreviewSize(300, 300)
 camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 camRgb.setInterleaved(False)
 camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-camRgb.setFps(40)
 
-# testing MobileNet DetectionNetwork
-detectionNetwork.setBlobPath(args.nnPath)
-detectionNetwork.setConfidenceThreshold(0.5)
-detectionNetwork.input.setBlocking(False)
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+# setting node configs
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# Align depth map to the perspective of RGB camera, on which inference is done
+stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+stereo.setOutputSize(monoLeft.getResolutionWidth(),
+                     monoLeft.getResolutionHeight())
+
+spatialDetectionNetwork.setBlobPath(args.nnPath)
+spatialDetectionNetwork.setConfidenceThreshold(0.5)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
 
 objectTracker.setDetectionLabelsToTrack([15])  # track only person
 # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
 objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
 # take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
-objectTracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
+objectTracker.setTrackerIdAssignmentPolicy(
+    dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
 
 # Linking
-camRgb.preview.link(detectionNetwork.input)
-objectTracker.passthroughTrackerFrame.link(xlinkOut.input)
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+camRgb.preview.link(spatialDetectionNetwork.input)
+objectTracker.passthroughTrackerFrame.link(xoutRgb.input)
+objectTracker.out.link(trackerOut.input)
 
 if fullFrameTracking:
+    camRgb.setPreviewKeepAspectRatio(False)
     camRgb.video.link(objectTracker.inputTrackerFrame)
+    objectTracker.inputTrackerFrame.setBlocking(False)
+    # do not block the pipeline if it's too slow on full frame
+    objectTracker.inputTrackerFrame.setQueueSize(2)
 else:
-    detectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
+    spatialDetectionNetwork.passthrough.link(objectTracker.inputTrackerFrame)
 
-detectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
-detectionNetwork.out.link(objectTracker.inputDetections)
-objectTracker.out.link(trackerOut.input)
+spatialDetectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
+spatialDetectionNetwork.out.link(objectTracker.inputDetections)
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
@@ -72,20 +101,19 @@ with dai.Device(pipeline) as device:
     startTime = time.monotonic()
     counter = 0
     fps = 0
-    frame = None
+    color = (255, 255, 255)
 
-    while(True):
+    while (True):
         imgFrame = preview.get()
         track = tracklets.get()
 
-        counter+=1
+        counter += 1
         current_time = time.monotonic()
-        if (current_time - startTime) > 1 :
+        if (current_time - startTime) > 1:
             fps = counter / (current_time - startTime)
             counter = 0
             startTime = current_time
 
-        color = (255, 0, 0)
         frame = imgFrame.getCvFrame()
         trackletsData = track.tracklets
         for t in trackletsData:
@@ -100,12 +128,24 @@ with dai.Device(pipeline) as device:
             except:
                 label = t.label
 
-            cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.putText(frame, t.status.name, (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+            cv2.putText(frame, str(label), (x1 + 10, y1 + 20),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(
+                frame, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, t.status.name, (x1 + 10, y1 + 50),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.rectangle(frame, (x1, y1), (x2, y2),
+                          color, cv2.FONT_HERSHEY_SIMPLEX)
 
-        cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+            cv2.putText(frame, f"X: {int(t.spatialCoordinates.x)} mm",
+                        (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"Y: {int(t.spatialCoordinates.y)} mm",
+                        (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"Z: {int(t.spatialCoordinates.z)} mm",
+                        (x1 + 10, y1 + 95), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+
+        cv2.putText(frame, "NN fps: {:.2f}".format(
+            fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
 
         cv2.imshow("tracker", frame)
 
